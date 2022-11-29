e7aca3ca309439884d46a1d5b5f302967b16cab5
havidtech <76119744+havidtech@users.noreply.github.com>
2022-11-29 01:50:25 +0100

    feat: add support for listing payment tokens

--- packages/hardhat/contracts/IMethCreate.sol
+++ packages/hardhat/contracts/IMethCreate.sol
@@ -11,11 +11,7 @@ interface IMethCreate {
     error NotSellable();
     error InsufficientPayment();
 
-    event TokenUpdated(
-        uint indexed tokenId,
-        uint indexec price,
-        bool indexed sellable
-    );
+    event TokenUpdated(uint indexed tokenId, uint indexed price, bool indexed sellable);
 
     event TokenSold(
         address indexed seller,
@@ -24,4 +20,26 @@ interface IMethCreate {
         address paymentToken,
         uint time
     );
+
+    event SellerAddedPaymentTokens(address indexed seller, address[] indexed tokens);
+
+    event SellerRemovedPaymentTokens(address indexed seller, address[] indexed tokens);
+
+    function safeMint(address to, string memory uri, uint price, bool sellable) external;
+
+    function setPrice(uint tokenId, uint price) external;
+
+    function setSellable(uint tokenId, bool sellable) external;
+
+    function buyTokenWithEth(uint tokenId, uint newPrice, bool sellable) external payable;
+
+    function addToMyPaymentTokens(address[] calldata tokens) external;
+
+    function removeFromMyPaymentTokens(address[] calldata tokens) external;
+
+    function getSellersPaymentToken(address seller) external view returns (address[] memory tokens);
+
+    function setTokenInfo(uint tokenId, uint price, bool sellable) external;
+
+    function tokenURI(uint256 tokenId) external view returns (string memory);
 }
