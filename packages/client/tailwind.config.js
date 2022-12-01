9745331e2ef3bf4aeec3d21bac5ee5c07c0178fa
EnebeliEmmanuel <iemmaenebeli@gmail.com>
2022-12-01 19:08:23 +0100

    refactor

--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -1,43 +1,16 @@
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+	mode: "jit",
+	// purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
 	content: [
 		"./pages/**/*.{js,ts,jsx,tsx}",
 		"./components/**/*.{js,ts,jsx,tsx}",
 		"./utilities/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {
-			width: {
-				"7/10": "70%",
-			},
-			colors: {
-				"dark-blue": "#2d314d",
-				"lime-green": "#31d35c",
-				"bright-cyan": "#2bb7da",
-				"grayish-blue": "#9698a6",
-				"light-greyish-blue": "#f3f4f6",
-				"light-gray": "#fafafa",
-			},
-			zIndex: {
-				top: "9999",
-				"second-top": "9998",
-			},
-			backgroundImage: {
-				"desktop-bg": `url(../images/bg-intro-desktop.svg)`,
-			},
-			fontSize: {
-				"section-title": "40px",
-			},
-			flexGrow: {
-				2: "2",
-			},
-		},
-		screens: {
-			sm: "639px",
-			md: "767px",
-			lg: "1023px",
-			xl: "1279px",
-			"2xl": "1535px",
-		},
+	  extend: {
+		
+	  },
 	},
-	plugins: [],
-};
+}
