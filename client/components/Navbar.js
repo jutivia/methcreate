b3542430f26d716ead1253a4d6085e04280cdd31
EnebeliEmmanuel <iemmaenebeli@gmail.com>
2022-12-02 20:31:15 +0100

    refactor

---
+++ client/components/Navbar.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React, { useState } from "react";
+import {Logo} from "../images";
+
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import Image from 'next/image'
+const Navbar = () => {
+	const [isOpenNav, setIsOpenNav] = useState(false);
+
+	return (
+		<nav className='w-full bg-white shadow-sm fixed top-0 z-top'>
+		
+		</nav>
+	);
+};
+
+export default Navbar;
